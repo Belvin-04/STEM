21004d5cf9da46cef0943fdbd829930732e89599
Belvin <belvinnadar527@gmail.com>
2021-03-03 23:47:17 +0530

    2 variable k-map solved using only if conditions

--- app/src/main/java/com/belvin/stem/KMapSolver.kt
+++ app/src/main/java/com/belvin/stem/KMapSolver.kt
@@ -1,5 +1,6 @@
 package com.belvin.stem
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,6 +15,7 @@ class KMapSolver : AppCompatActivity() {
     lateinit var abText:TextView
     lateinit var baText:TextView
     lateinit var bbText:TextView
+    lateinit var answer:TextView
 
     lateinit var aaValue:EditText
     lateinit var abValue:EditText
@@ -33,6 +35,7 @@ class KMapSolver : AppCompatActivity() {
         abText = findViewById(R.id.abText)
         baText = findViewById(R.id.baText)
         bbText = findViewById(R.id.bbText)
+        answer = findViewById(R.id.answer)
 
         aaValue = findViewById<TextInputLayout>(R.id.aa).editText!!
         abValue = findViewById<TextInputLayout>(R.id.ab).editText!!
@@ -61,6 +64,106 @@ class KMapSolver : AppCompatActivity() {
         }
     }
     fun solve(){
+        var aa = aaText.text.toString()
+        var ab = abText.text.toString()
+        var ba = baText.text.toString()
+        var bb = bbText.text.toString()
 
+        resetColour()
+
+        if(aa == "0" && ab == "0" && ba == "0" && bb == "0"){
+            answer.text = "0"
+        }
+        else if(aa == "0" && ab == "0" && ba == "0" && bb == "1"){
+            answer.text = "AB"
+            bbText.setBackgroundColor(Color.GRAY)
+
+        }
+        else if(aa == "0" && ab == "0" && ba == "1" && bb == "0"){
+            answer.text = "AB'"
+            baText.setBackgroundColor(Color.GRAY)
+        }
+        else if(aa == "0" && ab == "0" && ba == "1" && bb == "1"){
+            answer.text = "A"
+            baText.setBackgroundColor(Color.GRAY)
+            bbText.setBackgroundColor(Color.GRAY)
+        }
+        else if(aa == "0" && ab == "1" && ba == "0" && bb == "0"){
+            answer.text = "A'B"
+            abText.setBackgroundColor(Color.GRAY)
+        }
+        else if(aa == "0" && ab == "1" && ba == "0" && bb == "1"){
+            answer.text = "B"
+            abText.setBackgroundColor(Color.GRAY)
+            bbText.setBackgroundColor(Color.GRAY)
+
+        }
+        else if(aa == "0" && ab == "1" && ba == "1" && bb == "0"){
+            answer.text = "A'B + AB'"
+            abText.setBackgroundColor(Color.GRAY)
+            baText.setBackgroundColor(Color.CYAN)
+        }
+        else if(aa == "0" && ab == "1" && ba == "1" && bb == "1"){
+            answer.text = "A + B"
+
+            abText.setBackgroundColor(Color.GRAY)
+            baText.setBackgroundColor(Color.GRAY)
+            bbText.setBackgroundColor(Color.GRAY)
+
+        }
+        else if(aa == "1" && ab == "0" && ba == "0" && bb == "0"){
+            answer.text = "A'B'"
+            aaText.setBackgroundColor(Color.GRAY)
+
+        }
+        else if(aa == "1" && ab == "0" && ba == "0" && bb == "1"){
+            answer.text = "A'B' + AB"
+            aaText.setBackgroundColor(Color.GRAY)
+            bbText.setBackgroundColor(Color.CYAN)
+        }
+        else if(aa == "1" && ab == "0" && ba == "1" && bb == "0"){
+            answer.text = "B'"
+            aaText.setBackgroundColor(Color.GRAY)
+            baText.setBackgroundColor(Color.GRAY)
+
+        }
+        else if(aa == "1" && ab == "0" && ba == "1" && bb == "1"){
+            answer.text = "B' + A"
+            aaText.setBackgroundColor(Color.GRAY)
+            baText.setBackgroundColor(Color.GRAY)
+            bbText.setBackgroundColor(Color.GRAY)
+        }
+        else if(aa == "1" && ab == "1" && ba == "0" && bb == "0"){
+            answer.text = "A'"
+            aaText.setBackgroundColor(Color.GRAY)
+            abText.setBackgroundColor(Color.GRAY)
+        }
+        else if(aa == "1" && ab == "1" && ba == "0" && bb == "1"){
+            answer.text = "A' + B"
+            aaText.setBackgroundColor(Color.GRAY)
+            abText.setBackgroundColor(Color.GRAY)
+            bbText.setBackgroundColor(Color.GRAY)
+        }
+        else if(aa == "1" && ab == "1" && ba == "1" && bb == "0"){
+            answer.text = "A' + B'"
+            aaText.setBackgroundColor(Color.GRAY)
+            abText.setBackgroundColor(Color.GRAY)
+            baText.setBackgroundColor(Color.GRAY)
+        }
+        else{
+            answer.text = "1"
+            aaText.setBackgroundColor(Color.GRAY)
+            abText.setBackgroundColor(Color.GRAY)
+            baText.setBackgroundColor(Color.GRAY)
+            bbText.setBackgroundColor(Color.GRAY)
+        }
+
+    }
+
+    fun resetColour(){
+        aaText.setBackgroundColor(Color.WHITE)
+        abText.setBackgroundColor(Color.WHITE)
+        baText.setBackgroundColor(Color.WHITE)
+        bbText.setBackgroundColor(Color.WHITE)
     }
 }
